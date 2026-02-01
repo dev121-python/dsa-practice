ec6679ff45c56100598bd0c989d7d76ce3d54457
dev121-python <mittaldivyang211@gmail.com>
2026-02-01 21:00:39 +0530

    created hashtable / completed

--- Data-Structures/Hashing/Hash_table_chaining.py
+++ Data-Structures/Hashing/Hash_table_chaining.py
@@ -1,10 +1,23 @@
+import random
 class Hash_table:
-    def __init__(self):
-        self.buckets = [[]for i in range (10)]
+    def __init__(self,capacity=800):
+        self.buckets = [[]for i in range (capacity)]
+        self.capacity = capacity
+        self.size = 0
+        self.max_load = 0.75
+        self.p = 2**56+1
+        self.a = random.randrange(1,self.p)
+        self.b = random.randrange(0,self.p)
 
+    def _hash(self,key):
+        return (((self.a*hash(key )+self.b)%self.p)%self.capacity)
+    
 
     def put(self,key,value):
-        index = key%len(self.buckets)
+        if (self.size+1)/self.capacity > self.max_load:
+            self.resize()
+
+        index = self._hash(key)
         bucket = self.buckets[index]
         for i,(k,v) in enumerate(bucket):
             if k == key:
@@ -12,12 +25,13 @@ class Hash_table:
                 return
         
         self.buckets[index].append((key,value))
+        self.size+=1
 
 
     def get(self,key):
-        index = key % len(self.buckets)
+        index = self._hash(key)
         if self.buckets[index]==[]:
-            return "none"
+            return None
         else :
             bucket = self.buckets[index]
             for k , v in bucket:
@@ -26,24 +40,39 @@ class Hash_table:
             return None
                 
     def delete(self,key):
-        index = key%len(self.buckets)
+        index = self._hash(key)
         bucket = self.buckets[index]
         for i,(k,v) in enumerate(bucket):
             if k == key:
                 bucket.pop(i)
+                self.size-=1
                 return True
         
         return False
+    def resize(self):
+        old_buckets = self.buckets
+        capacity = self.capacity*2
+        self.buckets = [[]for i in range (capacity)]
+        self.size = 0
+        for bucket in old_buckets:
+            for k,v in bucket:
+                self.put(k,v)
+    def _iter(self):
+        for bucket in self.buckets:
+            for k,v in bucket:
+                yield k
+
+    def __repr__(self):
+        pairs = []
+        for bucket in self.buckets:
+            for k, v in bucket:
+                pairs.append(f"{repr(k)}: {repr(v)}")
+        return "{" + ", ".join(pairs) + "}"
+
         
-        
+ht = Hash_table()
+ht.put(1, "a")
+ht.put(2, "b")
+ht.put("x", 10)
 
-hm  = Hash_table()
-
-hm.put(1, 10)
-hm.put(11, 110)   # collision with 1 if size = 10
-
-print(hm.get(1))   # 10
-print(hm.get(11))  # 110
-print(hm.delete(1)) 
-print(hm.get(1))  # none
-print (hm)
+print(ht)
